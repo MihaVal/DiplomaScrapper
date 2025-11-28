b954ba366d3da66c275d29dbdda9b669d2ba2674
MihaVal <michael2silver@gmail.com>
2025-11-28 13:45:45 +0100

    Test attempt on Quotes.toscrape.com

--- WebScrapper.py
+++ WebScrapper.py
@@ -1,2 +1,13 @@
 #Start of web scrapper diploma
 
+from bs4 import BeautifulSoup
+import requests
+
+scrapped_page = requests.get("https://quotes.toscrape.com")
+soup = BeautifulSoup(scrapped_page.text, "html.parser")
+
+quotes = soup.find_all("span", attrs={"class" : "text"})
+authors = soup.find_all("small", attrs={"class" : "author"})
+
+for quote, author in zip (quotes, authors):
+    print(quote.text + " - " + author.text)
