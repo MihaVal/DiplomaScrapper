4fdc4df9005eef9954236d6a08234d96617c66bb
MihaVal <michael2silver@gmail.com>
2025-11-30 20:16:56 +0100

    Working on a 24ur article

--- WebScrapper.py
+++ WebScrapper.py
@@ -3,11 +3,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-scrapped_page = requests.get("https://quotes.toscrape.com")
+scrapped_page = requests.get("https://www.24ur.com/sport/odbojka/pajenk-radnicki-bo-naredil-vse-da-nam-zagreni-zivljenje.html")
 soup = BeautifulSoup(scrapped_page.text, "html.parser")
 
-quotes = soup.find_all("span", attrs={"class" : "text"})
-authors = soup.find_all("small", attrs={"class" : "author"})
+quotes = soup.find_all("div", attrs={"class" : "comment__content"})
+author = soup.find_all("a", attrs={"class" : "comment__author"})
 
-for quote, author in zip (quotes, authors):
-    print(quote.text + " - " + author.text)
+for quote, author in zip (quotes, author):
+    print(author.text + " - " + quote.text)
