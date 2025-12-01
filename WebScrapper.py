e6d486a0e0c40e7360a230419949cd6f1029324c
MihaVal <michael2silver@gmail.com>
2025-12-01 23:05:53 +0100

    Updated ReadMe & added some comments

--- WebScrapper.py
+++ WebScrapper.py
@@ -3,7 +3,7 @@
 from bs4 import BeautifulSoup
 import requests
 
-scrapped_page = requests.get("https://www.24ur.com/novice/slovenija/pahor-razburja-s-kampanjo.html")
+scrapped_page = requests.get("https://www.24ur.com/articles/4482768/comments/from_comment_id/01KBDS85J4PZDY5C00AZGGWQEE")
 soup = BeautifulSoup(scrapped_page.text, "html.parser")
 
 #quotes = soup.find_all("div", attrs={"class" : "comment__content"})
@@ -19,20 +19,15 @@ for comment in comment_block:
     text_part = comment.find("div", attrs={"class" : "comment__content--body"})
     likes = comment.find("span", attrs={"class" : "comment-likes-positive"})
     dislikes = comment.find("span", attrs={"class" : "comment-likes-negative"})
-
+    print("Total comments scraped:", len(comment_block))    
     """
-    dodajaj stvari ki jih hočeš vidit drugo pa izpusti in se ne bi smelo 
-    pokazat na konc (npr tisti icons stuff) težava ki me bega trenutno
-    je da bi rad vseeno dobil podatek lajkov in dislajkov, brez tistega
-    icons stuffa zraven ali pa da bi to preimenoval da bi bilo jasno 
-    zakaj se gre(npr kaj je ta podatek) nekaj stvari pa tudi izpustil 
-    sigurno (vprašanje tudi a je pomemben podatek je koliko komentov je
-    v subthreadu drugih komentov ki pokaže engagement level glede 
-    na polarnost komentarja posameznega recimo)
+    250 komentarjev, like 200 jih manka??
     """
     author = author_part.get_text(strip=True) if author_part else ""
     text = text_part.get_text(" ", strip=True) if text_part else ""
     rating_plus = likes.get_text(strip=True) if likes else ""
     rating_minus = dislikes.get_text(strip=True) if dislikes else ""
-
+    
     print(f"{author}: {text} | Všečki/Nevšečki [+{rating_plus}/-{rating_minus}]")
+
+    
